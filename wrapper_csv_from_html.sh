1680de12416a71f92a41473b2996042831d2eba1
Gyanesh Shukla <142218807+shuklagyanesh21@users.noreply.github.com>
2024-04-26 15:52:45 +0530

    Update wrapper_csv_from_html.sh

--- wrapper_csv_from_html.sh
+++ wrapper_csv_from_html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prompt the user for the path to the as_result_complete folder
-read -p "Enter the path to the as_result_complete folder: " folder_path
+read -p "Enter the path to the folder containing all antismash results: " folder_path
 
 # Check if the folder exists
 if [ ! -d "$folder_path" ]; then
@@ -9,19 +9,22 @@ if [ ! -d "$folder_path" ]; then
   exit 1
 fi
 
-# Iterate through each folder inside as_result_complete
+> ../all_regions.csv
+
+echo "Directory Name, Region, Type, From, To, Most similar known cluster, miBiG Type, Similarity" >> ../all_regions.csv
+# Iterate through each organims folder inside antismash results folder
 for dir in "$folder_path"/*; do
   if [ -d "$dir" ]; then
     echo "Processing folder: $dir"
     # Navigate to the folder and run html_extraction.py
     cd "$dir" || exit
-    python /home/gyanesh/Scripts/csv_from_html.py
-    cat regions_info.csv >> ../all_regions.csv
-    rm regions_info.csv
+    python /home/gyanesh/Scripts/html_extraction.py
+    sort regions_info.csv | uniq > temp 
+    cat temp >> ../all_regions.csv
+    rm regions_info.csv temp
     # Navigate back to the original directory
     cd - || exit
   fi
 done
 
 echo "Script execution complete."
-
